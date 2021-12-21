59b4dee024bbfcfb3289464a5b8829126e0930cd
asrar bhat <asrarpsi@gmail.com>
2021-12-21 16:07:41 +0530

    problem solving

---
+++ problemSolving/Problem.java
@@ -0,0 +1,67 @@
+import java.util.*;
+public class Problem{
+	public static void main(String[] args){
+//		System.out.println("hello world");
+//		export CLASSPATH before running
+
+		//Arrays
+		int[] arr={1,2,3,4,5,6,7,8,9,0};
+//		System.out.println(arr.length);
+//		System.out.println(arr[7]);
+//		System.out.println(arr[44]);//exception
+		List<Integer> list=new ArrayList<>();
+		list.add(3);//takes only one
+		list.add(4);
+		list.add(5);
+		list.add(6);
+//		System.out.println(list);
+
+//		System.out.println(list);//all this to pop
+//		System.out.println(list.remove(list.size()-1));
+//		System.out.println(list);
+
+
+//		int[] arr2=arr.clone();//creating an copy for arrays
+//		arr2[0]=77;
+//		System.out.println(Arrays.toString(arr));
+//		System.out.println(Arrays.toString(arr2));
+
+//		List<Integer> list2=new ArrayList<>(list);//still don't forget the individual elements are reference type so be careful
+//		list2.add(77);
+//		list.set(0,34);
+//		System.out.println(list);
+//		System.out.println(list2);
+
+		//list to arrays and viceversa
+//		Integer[] arr3={4,3,2,4};
+//		List<Integer> list3=Arrays.asList(arr3);
+//		System.out.println(list3);
+//		Integer[] arr4=new Integer[list.size()];
+//		arr4=list.toArray(arr4);
+//		System.out.println(Arrays.toString(arr4));
+
+//		System.out.println(list);
+//		System.out.println(list.remove(0));
+//		list.remove(2);
+//		list.add(2,99);
+//		System.out.println(list);
+
+//		for(int i:arr){
+//			System.out.println(i);
+//		}
+
+//		for(Integer i: list){
+//			System.out.println(i);
+//		}
+//		System.out.println(arr[3]+" "+list.get(2));
+//		System.out.println(list.indexOf(5));//no index of for arr
+
+//		int[] arr8={4,3,5};
+//		Arrays.sort(arr8);
+//		System.out.println(Arrays.toString(arr8));//noway to reverse
+
+//		System.out.println(Arrays.toString(Arrays.copyOfRange(arr,2,5)));//slicing
+
+
+	}
+}
